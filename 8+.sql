2cdb1af4a7d5bb3a907335ce7169c109373260ec
GabyAM <gdgabyam@gmail.com>
2024-07-26 12:24:31 -0300

    Create 8+.sql

---
+++ 8+.sql
@@ -0,0 +1,55 @@
+-- 1
+SELECT A_STRONGLY_AGREE
+  FROM nss
+ WHERE question='Q01'
+   AND institution='Edinburgh Napier University'
+   AND subject='(8) Computer Science'
+
+-- 2
+SELECT institution, subject
+  FROM nss
+ WHERE question='Q15'
+   AND score >= 100
+
+-- 3
+SELECT institution,score
+  FROM nss
+ WHERE question='Q15'
+   AND score<50
+   AND subject='(8) Computer Science'
+
+-- 4
+SELECT subject, SUM(response)
+  FROM nss
+ WHERE question='Q22'
+   AND (subject='(8) Computer Science' OR subject = '(H) Creative Arts and Design')
+GROUP BY subject
+
+-- 5
+SELECT subject, SUM((A_STRONGLY_AGREE * response) / 100)
+  FROM nss
+WHERE question='Q22'
+   AND (subject='(8) Computer Science' OR subject = '(H) Creative Arts and Design')
+GROUP BY subject
+
+-- 6
+SELECT subject, ROUND(SUM(response*A_STRONGLY_AGREE) /SUM(response))
+  FROM nss
+WHERE question='Q22'
+   AND (subject='(8) Computer Science' OR subject = '(H) Creative Arts and Design')
+GROUP BY subject
+
+-- 7
+SELECT institution, ROUND(SUM(score * response) / SUM(response))
+  FROM nss
+ WHERE question='Q22'
+   AND (institution LIKE '%Manchester%')
+GROUP BY institution
+ORDER BY institution
+
+-- 8     ??
+SELECT institution, SUM(sample), SUM(CASE WHEN subject='(8) Computer Science' THEN sample ELSE 0 END) AS comp
+  FROM nss
+ WHERE question='Q01'
+   AND (institution LIKE '%Manchester%')
+GROUP BY institution
